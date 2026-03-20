26129319672ceae447bb83afdf5a5edefa18ba3c
Mikael Saha <mikaeli-95@hotmail.com>
2026-03-20 16:54:53 +0200

    korjattu virhe

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
 export default defineConfig({
-  base: "/ruotu/",
+  base: "/ruotu-app/",
   plugins: [react(),
     tailwindcss()
   ],
